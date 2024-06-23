cd700c137f5ebef9c378c429982d33ddc5a97330
VinitRTiwari <vinittiwari2004@gmail.com>
2024-06-23 20:39:25 +0530

    create

---
+++ calculator.py
@@ -0,0 +1,73 @@
+from tkinter import *
+a = Tk()
+a.geometry ( "450x500" )                          #Size of GUI application
+a.title("Calculator...")                          #Title of GUI application
+a.configure({"bg": "gray"})                       #Background color for GUI application
+expression = ""
+text_show = StringVar()
+
+#function define input by clicking button
+
+def click(num):
+    global expression
+    expression = expression + str(num)
+    text_show.set(expression)
+
+#function that clear display screen
+
+def clear():
+    global expression
+    expression = ""
+    text_show.set("")
+
+#function that handle exception and display output
+
+def equal():
+    global expression
+    try:
+        output = str(eval(expression))
+        text_show.set(output)
+        expression = ""
+    except:
+        text_show.set("ERROR")
+        expression = ""
+    
+ #Screen to show output   
+e1 = Entry(a, textvariable = text_show ,bg="black",fg="orange",font=("Helvetica",29,"bold"))
+e1.grid(row=0,column=0,columnspan = 4)
+#Buttons of calculator....
+b1 = Button(a,text = "1",command=lambda : click(1),height=5,width=16,bg="gray",fg="orange")
+b1.grid(row=4,column=0)
+b2 = Button(a,text = "2",command=lambda : click(2),height=5,width=16,bg="gray",fg="orange")
+b2.grid(row=4,column=1)
+b3 = Button(a,text = "3",command=lambda : click(3),height=5,width=16,bg="gray",fg="orange")
+b3.grid(row=4,column=2)
+bAdd = Button(a,text = "+",command=lambda : click("+"),height=5,width=10,bg="gray",fg="orange")
+bAdd.grid(row=4,column=3)
+b4 = Button(a,text = "4",command=lambda : click(4),height=5,width=16,bg="gray",fg="orange")
+b4.grid(row=3,column=0)
+b5 = Button(a,text = "5",command=lambda : click(5),height=5,width=16,bg="gray",fg="orange")
+b5.grid(row=3,column=1)
+b6 = Button(a,text = "6",command=lambda : click(6),height=5,width=16,bg="gray",fg="orange")
+b6.grid(row=3,column=2)
+bSub = Button(a,text = "-",command=lambda : click("-"),height=5,width=10,bg="gray",fg="orange")
+bSub.grid(row=3,column=3)
+b7 = Button(a,text = "7",command=lambda : click(7),height=5,width=16,bg="gray",fg="orange")
+b7.grid(row=2,column=0)
+b8 = Button(a,text = "8",command=lambda : click(8),height=5,width=16,bg="gray",fg="orange")
+b8.grid(row=2,column=1)
+b9 = Button(a,text = "9",command=lambda : click(9),height=5,width=16,bg="gray",fg="orange")
+b9.grid(row=2,column=2)
+bMulti = Button(a,text = "*",command=lambda : click("*"),height=5,width=10,bg="gray",fg="orange")
+bMulti.grid(row=2,column=3)
+bClear = Button(a,text = "C", command=lambda: clear(),height=5,width=51,bg="gray",fg="orange")
+bClear.grid(row=1,column=0,columnspan=3)
+bDiv = Button(a,text = "/",command=lambda : click("/"),height=5,width=10,bg="gray",fg="orange")
+bDiv.grid(row=1,column=3)
+b0 = Button(a,text = "0",command=lambda : click(0),height=5,width=34,bg="gray",fg="orange")
+b0.grid(row=5,column=0,columnspan=2)
+bDot = Button(a,text = ".",command=lambda : click("."),height=5,width=16,bg="gray",fg="orange")
+bDot.grid(row=5,column=2)
+bEqual = Button(a,text = "=",command=lambda: equal(),height=5,width=10,bg="gray",fg="orange")
+bEqual.grid(row=5,column=3)
+a.mainloop()
